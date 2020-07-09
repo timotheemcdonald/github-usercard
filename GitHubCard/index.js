52f261ac197fa3e4d32642ca82ed8375165a0851
timotheemcdonald <timotheemcdonald@gmail.com>
2020-07-09 16:49:08 -0400

    adding my files for the day

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,18 @@
+import axios from 'axios'
+
+let nameVar = 'https://api.github.com/users/timotheemcdonald'
+
+
+console.log(nameVar)
+console.log(axios.get(nameVar))
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,7 +38,90 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
+function cardMaker(object){
+  const card = document.createElement('div')
+  const portrait = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const name = document.createElement('h3')
+  const userName = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const profilePage = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
+
+  card.appendChild(portrait)
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(name)
+  cardInfo.appendChild(userName)
+  cardInfo.appendChild(location)
+  cardInfo.appendChild(profile)
+  profile.appendChild(profilePage)
+  cardInfo.appendChild(followers)
+  cardInfo.appendChild(following)
+  cardInfo.appendChild(bio)
+
+  card.className = 'card'
+  cardInfo.className = 'card-info'
+  name.className = 'name'
+  userName.className = 'username'
+
+  portrait.src = object.avatar_url
+  name.textContent = object.name
+  userName.textContent = object.login
+  location.textContent = object.location
+  followers.textContent = object.followers
+  following.textContent = object.following
+  bio.textContent = object.bio
+
+  return card
+}
+
+let cardS = document.querySelector('.cards')
+// cardS.appendChild(cardMaker(nameVar))
+
+axios.get(nameVar)
+.then(function (value){
+  console.log('inside the function happy path', nameVar)
+let newCard = value.data
+cardS.appendChild(cardMaker(newCard))
+console.log('happy path')
+})
+.catch(function (error){
+console.log('sad path')
+})
+
+const followersArray = ['tetondan','dustinmyers','justsml','luishrd','bigknell'];
+
+followersArray.forEach( value => {
+
+  const info = `https://api.github.com/users/${value}`
+  axios.get(info)
+  .then(function (object){
+    console.log('happy follower path start')
+    let newCardS = object.data
+    cardS.appendChild(cardMaker(newCardS))
+    console.log('happy follower path finish')
+  })
+  .catch(function (error){
+    console.log('sad follower path')
+  })
+
+})
+
+// function followersTest(fName){
+  
+//   const followerName =  `https://api.github.com/users/${fName}`
+//   axios.get(followerName)
+
+//   followersArray.forEach(info => {
+//     const cards = cardMaker(info)
+//     cardS.appendChild(cards)
+//   })
+// }
+
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
